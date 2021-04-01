1eaaa61a40e485f5f606d5c68a518f5bd304e63d
Surabhi2481 <surabhibbsr2481@gmail.com>
2021-04-01 10:38:51 +0530

    implemented logging in service layer

--- src/main/java/com/surabhi/flightreservation/services/ReservationServiceImpl.java
+++ src/main/java/com/surabhi/flightreservation/services/ReservationServiceImpl.java
@@ -1,8 +1,11 @@
 package com.surabhi.flightreservation.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.surabhi.flightreservation.controllers.ReservationController;
 import com.surabhi.flightreservation.dto.ReservationRequest;
 import com.surabhi.flightreservation.entities.Flight;
 import com.surabhi.flightreservation.entities.Passenger;
@@ -31,28 +34,37 @@ public class ReservationServiceImpl implements ReservationService {
 	@Autowired
 	EmailUtil emailUtil;
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReservationServiceImpl.class);
+	
 	@Override
 	public Reservation bookFlight(ReservationRequest request) {
+		
+		LOGGER.info("Inside bookFlight()");
 		Long flightId = request.getFlightId();
 		Flight flight = flightRepository.findById(flightId).get();
+		
+		LOGGER.info("Fetching the flight of flight Id : "+flightId);
 		
 		Passenger passenger = new Passenger();
 		passenger.setFirstName(request.getPassengerFirstName());
 		passenger.setLastName(request.getPassengerLastName());
 		passenger.setPhone(request.getPassengerPhone());
 		passenger.setEmail(request.getPassengerEmail());
+		LOGGER.info("Saving the passenger "+passenger);
 		Passenger savedPassenger = passengerRepository.save(passenger);
 		
 		Reservation reservation = new Reservation();
 		reservation.setFlight(flight);
 		reservation.setPassenger(savedPassenger);
 		reservation.setCheckedIn(false);
+		LOGGER.info("Saving the reservation "+reservation);
 		Reservation savedReservation = reservationRepository.save(reservation);
 		
 		String filePath = "Z:\\Docs/Reservation/reservation"+savedReservation.getId()+".pdf";
+		LOGGER.info("Generating the Itinerary");
 		pdfGenerator.generateItinerary(savedReservation, filePath);
 		emailUtil.sendItinenary(passenger.getEmail(), filePath);
-		
+		LOGGER.info("Emailing the Itinerary");
 		return savedReservation;
 	}
 
